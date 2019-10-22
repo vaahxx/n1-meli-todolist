7dc92b8bdbe4d3d8a81045f10156a842b5134869
valentina <vaahxx@gmail.com>
2019-10-22 20:20:20 -0300

    rotas opcionais funcionando

--- src/controller/tarefasController.js
+++ src/controller/tarefasController.js
@@ -11,3 +11,15 @@ exports.getTarefasById = (req, res) => {
     const tarefaUnica = tarefasJson.find(e => e.id == idURL);
     res.status(200).send(tarefaUnica);
 };
+
+exports.getTarefasConcluidas = (req, res) => {
+    const tarefasConcluidas = tarefasJson.filter(e => e.concluido == "true");
+    res.status(200).send(tarefasConcluidas);
+};
+
+exports.getTarefasByName = (req, res) => {
+    // pega o valor do nome na url da request
+    const nomeUrl = req.params.nome;
+    const tarefasPessoa = tarefasJson.filter(e => e.nomeColaborador == nomeUrl);
+    res.status(200).send(tarefasPessoa);
+};
--- src/routes/tarefasRoutes.js
+++ src/routes/tarefasRoutes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const controller = require('../controller/tarefasController');
 
 router.get('/tarefas/', controller.getTarefas);
+router.get('/tarefas/concluidos/filtrar', controller.getTarefasConcluidas);
+router.get('/tarefas/:nome/buscar', controller.getTarefasByName);
 router.get('/tarefas/:id', controller.getTarefasById);
 
 module.exports = router;
